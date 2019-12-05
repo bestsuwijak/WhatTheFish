7755a98331509212fe7b346398db084a4092a609
bestsuwijak <42570047+bestsuwijak@users.noreply.github.com>
2019-12-06 02:55:55 +0700

    add popupshow

--- app/src/main/java/buu/informatics/s59160141/whatthefish/MainFragment.kt
+++ app/src/main/java/buu/informatics/s59160141/whatthefish/MainFragment.kt
@@ -1,19 +1,27 @@
 package buu.informatics.s59160141.whatthefish
 
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
 import buu.informatics.s59160141.whatthefish.databinding.FragmentMainBinding
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.popup_main.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
 
+    internal lateinit var viewPager: ViewPager
     lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,10 +34,23 @@ class MainFragment : Fragment() {
         )
 
         binding.buttonInformation.setOnClickListener{
-
+            showPopup()
         }
 
         return binding.root
+    }
+
+    private fun showPopup() {
+//        viewPager = LayoutInflater.from(context).inflate(R.layout.popup_main, null).viewPager
+//        val adapter = context?.let { ViewPagerMain(it) }
+//        viewPager.adapter = adapter
+
+        val mDialogView = LayoutInflater.from(context).inflate(R.layout.popup_main, null)
+        val mBuilder = AlertDialog.Builder(context).setView(mDialogView)
+        val mAlertDialog = mBuilder.show()
+        mDialogView.buttonClose.setOnClickListener(){
+            mAlertDialog.dismiss()
+        }
     }
 
 }
---
+++ app/src/main/java/buu/informatics/s59160141/whatthefish/ViewPagerMain.kt
@@ -0,0 +1,41 @@
+package buu.informatics.s59160141.whatthefish
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+class ViewPagerMain(private val context: Context) : PagerAdapter(){
+    private var layoutInflater:LayoutInflater? = null
+    private val images = arrayOf(R.drawable.popupmain1,R.drawable.popupmain2
+                            ,R.drawable.popupmain3,R.drawable.popupmain4)
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun getCount(): Int {
+        return images.size
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val v = layoutInflater!!.inflate(R.layout.popup_main, null)
+        val image = v.findViewById<View>(R.id.image_view) as ImageView
+        image.setImageResource(images[position])
+
+        val vp = container as ViewPager
+        vp.addView(v,0)
+        return v
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val vp = container as ViewPager
+        val v = `object` as View
+        vp.removeView(v)
+    }
+
+}
