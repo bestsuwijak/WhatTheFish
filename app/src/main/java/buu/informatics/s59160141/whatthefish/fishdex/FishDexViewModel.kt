31dc93bb9139fd73c8a649695ed42804aebaf31e
teerawatgithub <59160069@go.buu.ac.th>
2020-02-14 12:00:48 +0700

    fishdex2 v2 can get real data and use real background

---
+++ app/src/main/java/buu/informatics/s59160141/whatthefish/fishdex/FishDexViewModel.kt
@@ -0,0 +1,45 @@
+package buu.informatics.s59160141.whatthefish.fishdex
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import buu.informatics.s59160141.whatthefish.database.getDatabase
+import buu.informatics.s59160141.whatthefish.models.Fish
+import buu.informatics.s59160141.whatthefish.repository.FishesRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+class FishDexViewModel(application: Application, private val view: fishDexView) :
+    AndroidViewModel(application) {
+
+    private val fishesRepository = FishesRepository(getDatabase(application))
+    private val viewModelJob = SupervisorJob()
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    fun getFishes() {
+        viewModelScope.launch {
+            try {
+                val fishList = fishesRepository.fishes
+                view.setAdapterDataGrid(fishList)
+
+            } catch (e: Throwable) {
+                Timber.d("getFishes fail")
+            }
+        }
+    }
+
+
+    class Factory(val app: Application, val fishdexview: fishDexView) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(FishDexViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return FishDexViewModel(app, fishdexview) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
--- app/src/main/java/buu/informatics/s59160141/whatthefish/fishdex/Fishdex2Activity.kt
+++ app/src/main/java/buu/informatics/s59160141/whatthefish/fishdex/Fishdex2Activity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +17,13 @@ import kotlinx.android.synthetic.main.activity_fishdex2.*
 class Fishdex2Activity : AppCompatActivity(), fishDexView {
 
     private lateinit var binding: ActivityFishdex2Binding
+    private val viewModel: FishDexViewModel by lazy {
+        val activity = requireNotNull(this) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        ViewModelProviders.of(this, FishDexViewModel.Factory(activity.application, this))
+            .get(FishDexViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,40 +34,7 @@ class Fishdex2Activity : AppCompatActivity(), fishDexView {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_fishdex2)
         binding.gridFish.layoutManager = GridLayoutManager(this, 5)
         binding.gridFish.itemAnimator = DefaultItemAnimator() as RecyclerView.ItemAnimator?
-//        setContentView(R.layout.activity_fishdex2)
-
-        setAdapterDataGrid(listOf(Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-            TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552985925657F51_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552985925657F51_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552985925657F51_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0),
-            Fish(thNames = listOf("ttt"), engNames = listOf("sss"), scienceName = "tesdfah", appearance = "sfde", habitat = "asdfea", dissemination = "sesd", editedAt = "fsetgs", createdAt = "aesfa", textureImages = listOf(
-                TextureImage("asfse", 1.0)), images = listOf(Image("/images/1552989795661F1_shadow.png",1.3)), model = Model("asfe", 1.5), icon = Icon("asfe", 1.2), number = "F0", status = 0)))
+        viewModel.getFishes()
 
     }
 
