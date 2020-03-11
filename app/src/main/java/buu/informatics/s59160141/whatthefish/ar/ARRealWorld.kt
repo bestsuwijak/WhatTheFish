7e1e506ae7db17a22a1c9cabec48a03afc780fab
teerawatgithub <59160069@go.buu.ac.th>
2020-03-12 00:56:06 +0700

    multiple fish can animation and move on time

--- app/src/main/java/buu/informatics/s59160141/whatthefish/ar/ARRealWorld.kt
+++ app/src/main/java/buu/informatics/s59160141/whatthefish/ar/ARRealWorld.kt
@@ -25,17 +25,27 @@ import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_ar2.*
+import kotlinx.coroutines.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class ARRealWorld : AppCompatActivity() {
+
+    private val viewModelJob = SupervisorJob()
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
 
     lateinit var arFragment: ArFragment
     private lateinit var model: Uri
     private var renderable: ModelRenderable? = null
+    private var renderable2: ModelRenderable? = null
     private var animator: ModelAnimator? = null
+    private var animator2: ModelAnimator? = null
     var check = true
-    private var andy: F74? = null
+//    private var andy: F74? = null
     private var andy2: F74? = null
+    lateinit var andy: ArrayList<F74>
+    private var countAndy = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,8 +63,8 @@ class ARRealWorld : AppCompatActivity() {
                 placeObject(arFragment, anchor, model)
             }else{
 //                Log.i("test123",andy?.worldPosition.toString())
-                andy?.startWalking()
-                andy2?.startWalking()
+                andy[0].startWalking()
+                andy[1].startWalking()
             }
         }
 
@@ -74,16 +84,43 @@ class ARRealWorld : AppCompatActivity() {
             animator = ModelAnimator(data, modelRenderable)
             animator?.start()
         }
+
+        animator2?.let { it ->
+            if (it.isRunning) {
+                it.end()
+            }
+        }
+        renderable2?.let { modelRenderable ->
+            val data = modelRenderable.getAnimationData(name)
+            animator2 = ModelAnimator(data, modelRenderable)
+            animator2?.start()
+        }
     }
 
     private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
         if(check) {
+//            for (i in 0 until 2){
+                ModelRenderable.builder()
+                    .setSource(fragment.context, model)
+                    .build()
+                    .thenAccept {
+                        renderable = it
+                        addToScene(fragment, anchor, it, 0)
+                    }
+                    .exceptionally {
+                        val builder = AlertDialog.Builder(this)
+                        builder.setMessage(it.message).setTitle("Error")
+                        val dialog = builder.create()
+                        dialog.show()
+                        return@exceptionally null
+                    }
+
             ModelRenderable.builder()
                 .setSource(fragment.context, model)
                 .build()
                 .thenAccept {
-                    renderable = it
-                    addToScene(fragment, anchor, it)
+                    renderable2 = it
+                    addToScene(fragment, anchor, it, 1)
                 }
                 .exceptionally {
                     val builder = AlertDialog.Builder(this)
@@ -92,11 +129,12 @@ class ARRealWorld : AppCompatActivity() {
                     dialog.show()
                     return@exceptionally null
                 }
+//            }
                 check = false
         }
     }
 
-    private fun addToScene(fragment: ArFragment, anchor: Anchor, renderable: Renderable) {
+    private fun addToScene(fragment: ArFragment, anchor: Anchor, renderable: Renderable, i: Int) {
 
         val anchorNode = AnchorNode(anchor)
         val skeletonNode = SkeletonNode()
@@ -106,15 +144,25 @@ class ARRealWorld : AppCompatActivity() {
 //        node.worldRotation = Quaternion.axisAngle(Vector3(0f, 1f, 0f), 180f)
 
         //floating
-        node.worldPosition = Vector3(0f,0.17f,0f)
 
-        node.addChild(skeletonNode)
-        node.setParent(anchorNode)
-
-        fragment.arSceneView.scene.addChild(anchorNode)
-
-        andy = F74(node)
-        andy2 = F74(node)
+        viewModelScope.launch {
+            if (i == 0){
+                node.worldPosition = Vector3(0f,0.17f,0f)
+                node.addChild(skeletonNode)
+                node.setParent(anchorNode)
+                fragment.arSceneView.scene.addChild(anchorNode)
+                andy = arrayListOf(F74(node))
+                Log.i("test123", andy.size.toString())
+            }else{
+                delay(5000)
+                Log.i("test123", andy.size.toString())
+                node.worldPosition = Vector3(0f,0f,0f)
+                node.addChild(skeletonNode)
+                node.setParent(anchorNode)
+                fragment.arSceneView.scene.addChild(anchorNode)
+                andy.add(F74(node))
+            }
+        }
 
     }
 
